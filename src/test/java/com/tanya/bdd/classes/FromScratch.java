f6997dc7b62bb0c060c3dd79b6ac8ae22fb1806a
Tetiana.Malva <malva777@ukr.net>
2017-01-31 21:20:22 +0100

    Continue studding some basic stuff

--- src/test/java/com/tanya/bdd/classes/FromScratch.java
+++ src/test/java/com/tanya/bdd/classes/FromScratch.java
@@ -3,27 +3,37 @@ package com.tanya.bdd.classes;
 
 public class FromScratch {
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
-        int num = 7;
-        double d = 4.5;
-        d = 3.5;
-        float f = 4.5f;
-        char c = 'e';
-        String s1 = new String("Some text");
-        String s2 = "Add message here";
-        String s3 = s1 + s2;
-        String s = "I have " + num + " cookies";
-        System.out.println(s);
-        boolean b = true;
-        b = false;
-
-        boolean toBe = false;
-        b = toBe || !toBe;
-        if (b) {
-            System.out.println(toBe);
+//        System.out.println("Hello World!");
+//        int num = 7;
+//        double d = 4.5;
+//        d = 3.5;
+//        float f = 4.5f;
+//        char c = 'e';
+//        String s1 = new String("Some text");
+//        String s2 = "Add message here";
+//        String s3 = s1 + s2;
+//        String s = "I have " + num + " cookies";
+//        System.out.println(s);
+//        int a = 4;
+//        boolean b = a == 4;
+//        if (b) {
+//            System.out.println("It is true");
+//        }
+//        int c = 16;
+//        if (c == 19)
+//        {
+//            System.out.println("It is 16");
+//        }
+        int a = 7;
+        int b = 7;
+        int i = 0;
+        a = i + 4; i = i + 1;
+        if (a == b) {
+            System.out.println("true");
         }
-
+            else {
+            System.out.println("false");
+        }
 
     }
 }
