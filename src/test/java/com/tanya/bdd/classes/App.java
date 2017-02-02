e9c2dc2e70b06a0d04da5eefc5b1610090f1213c
Tetiana.Malva <malva777@ukr.net>
2017-02-02 21:52:30 +0100

    Continue studding some basic stuff

---
+++ src/test/java/com/tanya/bdd/classes/App.java
@@ -0,0 +1,17 @@
+package com.tanya.bdd.classes;
+
+public class App {
+    public static void main(String[] args) {
+        String a = "jak";
+        String a2 = "jak2";
+        int b = 4;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+        boolean f = true;
+        if (!a.equals(a2)) {
+            System.out.println("hello");
+        }
+    }
+}
