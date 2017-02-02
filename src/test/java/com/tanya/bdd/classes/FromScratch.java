98cfda0d60d94aa77c89754ce6c724d69967d3cd
Tetiana.Malva <malva777@ukr.net>
2017-02-02 21:13:58 +0100

    Continue studding some basic stuff

--- src/test/java/com/tanya/bdd/classes/FromScratch.java
+++ src/test/java/com/tanya/bdd/classes/FromScratch.java
@@ -1,6 +1,8 @@
 package com.tanya.bdd.classes;
 
 
+import org.apache.commons.lang3.ObjectUtils;
+
 public class FromScratch {
     public static void main(String[] args) {
 //        System.out.println("Hello World!");
@@ -24,16 +26,67 @@ public class FromScratch {
 //        {
 //            System.out.println("It is 16");
 //        }
-        int a = 7;
-        int b = 7;
-        int i = 0;
-        a = i + 4; i = i + 1;
-        if (a == b) {
-            System.out.println("true");
-        }
-            else {
-            System.out.println("false");
-        }
+//        int a;
+//        int b = 7;
+//        int i = 0;
+//        a = i + 1;
+//        if (a == b) {
+//            System.out.println("true");
+//        } else {
+//            System.out.println("false");
+//        }
+//    }
+//        int a = 5;
+//        int result = (a == 5) ? 7 : 6;
+//        System.out.println(result);
+////}
+        String a1 = "Wow";
+        String b1 = "Wow";
 
+        printReference(a1);
+        printReference(b1);
+
+        boolean r1 = (a1 == b1);
+        System.out.println(r1);
+
+        boolean r2 = a1.equals(b1);
+        System.out.println(r2);
+
+        String a2 = new String("Wow");
+        String b2 = new String("Wow");
+        printReference(a2);
+        printReference(b2);
+
+        boolean r3 = (a2 == b2);      // Ложь, так как a и b не один и тот же объект
+        System.out.println(r3);
+
+        boolean r4 = a2.equals(b2); // Истина, так как a и b логически равны
+        System.out.println(r4);
+
+//        int k;
+//        k = 5;
+//        if (k < 4) {
+//            k++;
+//        } else {
+//            k--;
+//        }
+//        System.out.println(k);
+//    }
+//    int a;
+//    a = 4;
+//        switch (a) {
+//            case 0:
+//                a++;
+//
+//            case 1:
+//                a--;
+//            default:
+//                a = a - 2;
+//            {
+//                System.out.println(a);
+    }
+
+    public static void printReference(String str) {
+        System.out.println("Memory reference: " + ObjectUtils.identityToString(str).replace("java.lang.String@", ""));
     }
 }
