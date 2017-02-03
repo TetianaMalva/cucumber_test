8100b878013019c784fb15431f8c6e8d3b557f1f
Tetiana.Malva <malva777@ukr.net>
2017-02-03 21:34:27 +0100

    Continue studding some basic stuff

--- src/test/java/com/tanya/bdd/classes/Arrays.java
+++ src/test/java/com/tanya/bdd/classes/Arrays.java
@@ -3,8 +3,8 @@ package com.tanya.bdd.classes;
 
 public class Arrays {
     public static void main(String[] args) {
-        //    int [] a = new int [4];
-//    int [] n = {13, 5, 6, 12, 12};
+        int [] t = new int [4];
+        int [] g = {13, 5, 6, 12, 12};
 
         int a = 78;
         int b = 7;
@@ -13,8 +13,7 @@ public class Arrays {
 
         if (a > b) {
             System.out.println("a bigger b");
-        }
-        if (a < b) {
+        } else if (a < b) {
             System.out.println("b bigger a");
         } else {
             System.out.println("a equal b");
