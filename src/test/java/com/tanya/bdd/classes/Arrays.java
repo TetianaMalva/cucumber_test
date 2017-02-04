eada6623f8f1988588cea9f33daa93658d56c58d
Tetiana.Malva <malva777@ukr.net>
2017-02-04 02:22:19 +0100

    Continue studding some basic stuff (while/if)

--- src/test/java/com/tanya/bdd/classes/Arrays.java
+++ src/test/java/com/tanya/bdd/classes/Arrays.java
@@ -1,15 +1,12 @@
 package com.tanya.bdd.classes;
 
-
 public class Arrays {
     public static void main(String[] args) {
-
-        int a = 38;
+        int a = 8;
         int b = 5;
-        int i = 0;
-        boolean f = a > b;
-        System.out.println(f);
-
+        int x = 1;
+        boolean l = a > b;
+        System.out.println(l);
         if (a > b) {
             System.out.println("a bigger b");
         } else if (a < b) {
@@ -17,11 +14,29 @@ public class Arrays {
         } else {
             System.out.println("a equal b");
         }
+        System.out.println("next");
         do {
-            System.out.println(a);
-            a = a - i;
-            i = i + 9;
+            System.out.println("a = " + a);
+            a = a - x;
+            x = x + 3;
         }
-            while (a > b);
+        while (a > b);
+
+        System.out.println("next");
+        int f = 5;
+        int result = 1;
+        for (int i = 1; i <= f; i++)
+            result = result * i;
+        {
+            System.out.println("!5 = " + result);
+        }
+        System.out.println("next");
+        int y = 7;
+        int rez = 1;
+        while (y > 0)
+            rez = rez * y--;
+        {
+            System.out.println("!7 = " + rez);
+        }
     }
 }
